0e381bd29a08f976946ef541c5f270a0627c6ca9
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2009-09-21 07:37:29 +0000

    Moved AMF messagebroker to modules

---
+++ ZendFramework/application/modules/messagebroker/Bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+class Messagebroker_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+}
---
+++ ZendFramework/application/modules/messagebroker/controllers/AmfController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plsdad index controller.
+ * 
+ * @uses Zend_Controller_Action
+ *
+ */
+class Messagebroker_AmfController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        /* Initialize action controller here */
+    	$this->_helper->viewRenderer->setNoRender(true);
+    	$this->_helper->layout->disableLayout();
+    }
+
+    public function indexAction()
+    {
+        // action body
+        $server = new Zend_Amf_Server();
+		$server->addDirectory(APPLICATION_PATH . '/modules/messagebroker/services/');
+		$server->setClassMap("FileVO", "FileVO");
+		echo($server->handle());
+    }
+}
+
