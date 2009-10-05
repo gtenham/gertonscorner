f789a95c20e2391b821c21003c988f2628c54b3e
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2009-10-05 18:50:47 +0000

    Changed Zend AMF messagebroker service

--- ZendFramework/application/modules/messagebroker/services/RemoteFile.php
+++ ZendFramework/application/modules/messagebroker/services/RemoteFile.php
@@ -7,7 +7,7 @@ class RemoteFile {
 * @param  $file as a Messagebroker_Model_FileVO Object
 * @return string
 **/ 
-   public function upload(Messagebroker_Model_FileVO $file) { 
+   public function upload($file) { 
       $data = $file->filedata;
       file_put_contents( APPLICATION_PATH . '/../tmp/' . $file->filename, $data);
       return 'File: ' . $file->filename .' Uploaded '; 
