6998fa96569c984166cbad5c3c8ec0e4865af53f
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2009-10-20 20:25:03 +0000

    Added comment block

--- phplib/GcLib/Db/OraException.php
+++ phplib/GcLib/Db/OraException.php
@@ -1,5 +1,10 @@
 <?php
 Class GcLib_Db_OraException extends Exception {
+	/*
+	 * Override the default Exception constructor
+	 * 
+	 * @param $ocierror oci_error type error
+	 */
 	public function __construct($ocierror) {
 		$code = $ocierror['code'];
 		$message = $ocierror['message'];
