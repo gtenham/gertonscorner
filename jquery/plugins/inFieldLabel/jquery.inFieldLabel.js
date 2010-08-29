db1b57e2833c9ac4a25c74388ac2750a4ea6dbf1
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2010-08-29 19:40:10 +0000

    Added inFieldLabel jQuery plugin

---
+++ jquery/plugins/inFieldLabel/jquery.inFieldLabel.js
@@ -0,0 +1,82 @@
+ /*
+ * inFieldLabel
+ * 
+ * Copyright (c) 2010 Gerton ten Ham
+ * 
+ * See for demo and example code: http://gertonscorner.wordpress.com
+ *
+ * Version 0.1   -   29-08-2010
+ *
+ * This inFieldLabel jQuery plug-in is dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+(function($){
+	$.fn.inFieldLabel = function(options) {
+		var defaults = { 
+			labelText: "Enter Name Here",
+			useAttribute: false
+	  	};  
+	 	var opts = $.extend(defaults, options);
+		
+		var input = $(this);
+		var inlineLabel = opts.labelText;
+		if (opts.useAttribute && (typeof opts.useAttribute == "string")) {
+			inlineLabel = input.attr(opts.useAttribute);
+			// Override default browser behaviour for useAttribute "title"
+			if (opts.useAttribute == "title") {
+				input.hover(function(e){											 							   
+					input.attr("title","");									  				
+				},
+				function(){	
+					input.attr("title",inlineLabel);
+				});	
+			}
+		}
+		var labelContainer = $('<span class="inline-label">'+inlineLabel+'</span>');
+		
+		// Modify the DOM by adding placeholder label
+		input.before(labelContainer);
+		
+		labelContainer.click(function() {
+			input.focus();
+		});
+		
+		showInlineLabel();
+		
+		input
+			.focus(function(){
+                showInlineLabel();
+            })
+            .blur(function(){
+				showInlineLabel();
+            })
+			.keydown(function(e) {
+				// All keystrokes except [shift]
+				if (e.keyCode != 16) {
+				   labelContainer.hide();
+				}
+				// Check on del key, if no data left, force show label
+				if (e.keyCode == 46 && input.val().length <= (this.selectionEnd-this.selectionStart)) {
+					showInlineLabel(true);
+				}
+				// Check on backspace key, if no data left, force show label
+				if (e.keyCode == 8 && input.val().length <= 1) {
+					showInlineLabel(true);
+				}
+			})
+			.change(function(){
+                showInlineLabel();
+            })
+            ;
+		function showInlineLabel(force) {
+			if ( force || input.val() === '') {
+				labelContainer.show();
+			} else {
+				labelContainer.hide();
+			}
+		}
+		
+	};
+	
+})(jQuery); 
