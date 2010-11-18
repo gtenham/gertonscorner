c4c56579899934713695ecf10a55fcacb17db9b4
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2010-11-18 21:10:13 +0000

    Added new Dao gateway for using MongoDb

---
+++ phplib/GcLib/Dao/MongoDbGateway.php
@@ -0,0 +1,18 @@
+<?php
+class GcLib_Dao_MongoDbGateway  extends GcLib_Dao_Gateway {
+	protected $_conn;
+	protected $_servername;
+	
+    public function __construct($options) {
+		$this->_options = $options;
+		$this->_conn = $this->getConnection();
+	}
+	
+	public function getConnection() {
+	   if (!$this->_conn) {
+	   	   $this->_servername = $this->_options["servername"];
+	   	   $this->_conn = new Mongo($this->_servername);
+	   	}
+		return $this->_conn;
+	}
+}
