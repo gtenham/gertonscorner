b8482e23d73ce8031fb53d3f451587dbf47b7b9e
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2010-11-18 22:43:50 +0000

    Added support for mongoDB connect options in the gateway class like persisted connections

--- phplib/GcLib/Dao/MongoDbGateway.php
+++ phplib/GcLib/Dao/MongoDbGateway.php
@@ -2,16 +2,19 @@
 class GcLib_Dao_MongoDbGateway  extends GcLib_Dao_Gateway {
 	protected $_conn;
 	protected $_servername;
+	protected $_connectoptions;
 	
     public function __construct($options) {
 		$this->_options = $options;
+		$this->_connectoptions = $options;
+		unset($this->_connectoptions["servername"]);
 		$this->_conn = $this->getConnection();
 	}
 	
 	public function getConnection() {
 	   if (!$this->_conn) {
 	   	   $this->_servername = $this->_options["servername"];
-	   	   $this->_conn = new Mongo($this->_servername);
+	   	   $this->_conn = new Mongo($this->_servername,$this->_connectoptions);
 	   	}
 		return $this->_conn;
 	}
