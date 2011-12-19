23a2c7a8ff174440e460ca695b58b2d0c1d4eaa8
gerton.tenham@yahoo.com <gerton.tenham@yahoo.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2011-12-19 15:46:43 +0000

    Make limitMaxlength plugin work on multiple textarea instances. - New callback function for initialisation - Modified demo to show remaining characters in span

--- jquery/plugins/limitMaxlength/jquery.limitMaxlength.js
+++ jquery/plugins/limitMaxlength/jquery.limitMaxlength.js
@@ -1,39 +1,68 @@
+/*
+ * limitMaxlength - jQuery Plugin
+ * Use maxlength attribute on textareas
+ *
+ * Examples and documentation at: http://gertonscorner.wordpress.com
+ * See for more examples the demo at: 
+ * http://gertonscorner.googlecode.com/svn/trunk/jquery/demo.html
+ *
+ * Version: 0.1 (19/12/2011)
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
 (function($){
-	$.fn.limitMaxlength = function(options){
-
-		var settings = jQuery.extend({
-			attribute: "maxlength",
-			onLimitCallback: function(){},
-			onEditCallback: function(){}
-		}, options);
+	$.fn.limitMaxlength = function(options) {
+		
+		var defaults = { 
+				attribute: "maxlength",
+				onInitCallback: function(elem, remaining){},
+				onLimitCallback: function(elem, remaining){},
+				onEditCallback: function(elem, remaining){}
+		  	};  
+		var settings = $.extend(defaults, options);
 	
-		// Initialise variables
-		var element = jQuery(this);
-		var maxlength = parseInt(element.attr(settings.attribute));
-		var length = element.val().length;
-		
-		// Event handler to limit the textarea
-		var onEdit = function(){
-			// Use timeout for settling focus, paste events
-			setTimeout(function() {
-				    length = element.val().length;
-					if ((maxlength - length) == 0) {
-						element.val(element.val().substr(0, maxlength));
-						settings.onLimitCallback.call(this,maxlength);
-					}
-					settings.onEditCallback.call(this,(maxlength - length));
-				},
-			100);
+		return this.each(function() {
+			// Initialise variables
+			var textarea = $(this);
+			var maxlength = parseInt(textarea.attr(settings.attribute));
+			var length = textarea.val().length;
+			settings.onInitCallback.call(this, textarea, (maxlength - length));
 			
-		};
-		
-		element.keyup(function(e) {
+			// Event handler to limit the textarea
+			function onEdit() {
+				
+				// Use timeout for settling focus, paste events
+				setTimeout(function() {
+						length = textarea.val().length;
+						if ((maxlength - length) == 0) {
+							textarea.val(textarea.val().substr(0, maxlength));
+							settings.onLimitCallback.call(this,textarea,maxlength);
+						}
+						settings.onEditCallback.call(this,textarea,(maxlength - length));
+					},
+					100);
+				
+			};
+			
+			
+			textarea
+				.keyup(function(e) {
 						// All keyCodes except [SHIFT], [CTRL] etc
 						if( (e.keyCode >= 46 || e.keyCode == 8 || e.keyCode == 32) ) {
 							onEdit();
 						}
 					})
-				.focus(onEdit)
-				.live('paste', onEdit);
+				.focus(function() {
+						onEdit();
+					})
+				.live('paste', function() {
+							onEdit();
+						});
+			
+			
+		});
+	
 	};
 })(jQuery);
