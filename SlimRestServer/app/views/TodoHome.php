665f5a38f0b690bdebc52398cc15c699d5d091be
gerton.tenham@gmail.com <gerton.tenham@gmail.com@cd238810-0b2c-11de-a6c4-5ba5c67cbe75>
2012-01-31 19:57:12 +0000

    Added html5 home page (based on html5 boilerplate)

--- SlimRestServer/app/views/TodoHome.php
+++ SlimRestServer/app/views/TodoHome.php
@@ -1,2 +1,49 @@
-<?php
-echo 'Running Slim app :: '.$name;
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  	<title><?php echo 'Running Slim app :: '.$name; ?></title>
+  	<meta name="description" content="Home view of Todo application">
+  	<meta name="author" content="Gerton ten Ham">
+
+  	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+  	<link rel="stylesheet" href="css/style.css">
+  
+  	<script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+
+  	<div id="container">
+    	<header>
+
+    	</header>
+    	<div id="main" role="main">
+
+    	</div>
+    	<footer>
+
+    	</footer>
+  	</div> <!--! end of #container -->
+
+
+	<!-- Grab javascript loader, with a protocol relative URL; fall back to local if offline -->
+  	<script type="application/javascript" src="//cdnjs.cloudflare.com/ajax/libs/labjs/2.0.3/LAB.min.js"></script>
+  	<script>window.$LAB || document.write('<script src="js/libs/LAB-2.0.3.min.js"><\/script>')</script>
+	<script type="application/javascript" src="/js/load.js"></script>
+	
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+  
+</body>
+</html>
